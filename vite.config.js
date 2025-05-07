844ca57298a2809faecb59e2f1c87f7a6a517dfb
Mattisbbn <mattisbbn@gmail.com>
2025-05-07 10:33:42 +0200

    Vite build configuration

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,22 @@ import { defineConfig } from 'vite';
 import fullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-    root: './',
+    // Ne pas définir `root` si le fichier `index.php` est à la racine
     plugins: [
-        fullReload(['src/**/*.php'])
+        fullReload(['src/**/*.php', './index.php']),
     ],
     build: {
-        outDir: 'build',
-        assetsDir: '.',
-        emptyOutDir: true
+        manifest: true,
+        rollupOptions: {
+            input: 'assets/js/main.js',
+        },
+        outDir: 'build', // pas besoin de ../build si tu es déjà à la racine
+        emptyOutDir: true,
     },
     server: {
         hmr: {
             host: 'localhost',
-            port: 5173
+            port: 5173,
         },
         host: 'localhost',
         port: 5173,
@@ -25,7 +28,8 @@ export default defineConfig({
                 'src/**/*.php',
                 'assets/**/*.js',
                 'assets/**/*.css',
-                'assets/**/*.html'
+                'assets/**/*.html',
+                './index.php',
             ]
         }
     }
