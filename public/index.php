60fac400e5443d254f70edf823b75f0def3510d0
Mattisbbn <mattisbbn@gmail.com>
2025-05-21 22:58:40 +0200

    upd

--- public/index.php
+++ public/index.php
@@ -3,6 +3,3 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 require_once __DIR__ . '/../src/Router/web.php';
-
-
-var_dump($_ENV);
